5e278f8960947525ae8160a2112009dbb6e6842c
soroushby <49848393+soroushby@users.noreply.github.com>
2020-09-07 11:37:18 -0700

    added back ground

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -6,6 +6,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
@@ -17,6 +19,8 @@ import { MatCardModule } from '@angular/material/card';
     MatButtonModule,
     MatListModule,
     MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   exports: [
     MatSidenavModule,
@@ -26,6 +30,8 @@ import { MatCardModule } from '@angular/material/card';
     MatButtonModule,
     MatListModule,
     MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
